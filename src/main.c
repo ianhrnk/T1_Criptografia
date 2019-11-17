f0ce3f9d8b867b9971892ecc084ac6e212143cb9
Ian Haranaka <ian.hrnk@gmail.com>
2019-11-17 12:45:17 -0400

    att main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "euclides.h"
-#include "num_aleatorio.h"
 #include "fabio.h"
 #include "patricia.h"
 #include "teodoro.h"
@@ -12,37 +10,37 @@
 // demais valores = 2^64 - 1 | tipo long int %li
 // P/ operacoes como multiplicacoes modulares = 2^127 - 1 | tipo __uint128_t 
 
+/*//////////////////////////////////////////////////////////////////
+//  IMPORTANTE: COMENTAR O CODIGO
+//////////////////////////////////////////////////////////////////*/
+
 int main(int argc, char **argv)
 {	
 	srand(time(NULL));
 	char tarefa = 'X';
-	unsigned long int p, q;
-	unsigned long long int n, s, v;
-	// Matrizes temporarias
-	__uint128_t A[2][3] = {0, 1, 0, 0, 0, 1};
-	unsigned long long int B[1][3];
+	int p, q;
+  long int n, s, v;
 
 	switch(*argv[1]) 
 	{
 		case 'T':
-			while (tarefa != 'T') {
+			while (tarefa != 'T')
+			{
 				scanf("%c", &tarefa);
 				
 				// Inicializar - Calcular n = pq
-				if (tarefa == 'I') {
-					scanf("%lu %lu", &p, &q);
+				if (tarefa == 'I')
+				{
+					scanf("%d %d", &p, &q);
 					n = teodoroInicializar(p, q);
-					printf("C %llu\n", n);
+					printf("C %ld\n", n);
 				}
 
 				// Autenticar - gerar v (publico) e s (privado)
-				// s^2*v congruente a 1 (mod n)
-				// TODO: Precisa arrumar, codigo baguncado 
-				else if (tarefa == 'A') { 
-					s = gerarNumAleatorio(n);
-					euclides(n, s*s, A, B);
-					printf("C %llu %llu\n", B[0][1], s);
-				}
+				else if (tarefa == 'A') {
+					teodoroAutenticar(n, &s, &v);
+          printf("C %ld %ld\n", v, s);
+        }
 
 				// Terminar
 				else if (tarefa == 'T')
@@ -51,12 +49,14 @@ int main(int argc, char **argv)
 			break;
 
 		case 'F':
-			while (tarefa != 'T') {
+			while (tarefa != 'T')
+			{
 				scanf("%c", &tarefa);
 				
 				// Identificar - verificar se n | s^2*v - 1
-				if (tarefa == 'I') {
-					scanf("%llu %llu %llu", &n, &s, &v);
+				if (tarefa == 'I')
+				{
+					scanf("%ld %ld %ld", &n, &s, &v);
 
 					if (fabioIdentificar(n, s, v))
 						printf("C\n");
