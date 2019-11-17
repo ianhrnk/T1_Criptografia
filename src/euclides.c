c9e0be04e07f8f9fe8712e2ae4e76fc86d2275f2
Ian Haranaka <ian.hrnk@gmail.com>
2019-11-17 12:44:49 -0400

    fix euclides

--- src/euclides.c
+++ src/euclides.c
@@ -1,56 +1,37 @@
 #include "euclides.h"
+#include <stdio.h>
 
-// TODO: Precisa arrumar, codigo instavel e baguncado
-// d = mdc(a,b) = sa + tb (sa + tb)
-// d = A[0][0]
-// t = A[0][1]
-// s = A[0][2]
-// OBS: Checar se a > n
-// TODO: Inicializar a matriz A
-// A = {b, 1, 0, a, 0, 1}
-// Verificar se y eh negativo. Se sim, somar com o modulo
-void euclides(__uint128_t a, __uint128_t b, __uint128_t A[2][3], unsigned long long int B[1][3]) {
-	__uint128_t aux = 0;
+long int euclides(long int a, long int b)
+{
+  __int128_t d1 = b*b, t1 = 1, s1 = 0, d2 = a, t2 = 0, s2 = 1, temp = 0;
 
-	if (a < b) {
-		A[0][0] = a;
-		A[1][0] = b;
-	}
-	else {
-		A[0][0] = b;
-		A[1][0] = a;
-	}
+  while (d1 != 0 && d2 != 0)
+  {
+    temp = d2 / d1;
+    d2 -= temp * d1;
+    t2 -= temp * t1;    
+    s2 -= temp * s1;
 
-	while (A[1][0] != 0 && A[0][0] != 0) {
-		aux = A[1][0] / A[0][0];
-
-		for (int i = 0; i < 3; ++i)
-			A[1][i] -= aux * A[0][i];
-
-		if (A[1][0] != 0 && A[0][0] != 0) {
-			aux = (A[0][0] / A[1][0]);
-			for (int i = 0; i < 3; ++i)
-				A[0][i] -=  aux * A[1][i];
-		}
-	}
-
-  if (A[0][0] == 0) {
-    B[0][0] = A[1][0];
-    B[0][2] = A[1][2];
-    if (a < b)
-      B[0][1] = A[1][2];
-    else
-      B[0][1] = A[1][1];    
-  }
-  else {
-    B[0][0] = A[0][0]; 
-    B[0][2] = A[0][2]; 
-    if (a < b)
-      B[0][1] = A[0][2];
-    else
-      B[0][1] = A[0][1];    
+    if (d1 != 0 && d2 != 0)
+    {
+      temp = d1 / d2;
+      d1 -= temp * d2;
+      t1 -= temp * t2;
+      s1 -= temp * s2;
+    }
   }
 
-  if (B[0][1] < 0)
-      B[0][1] += a;
+  // Verifica se o inverso multiplicativo é negativo.
+  if (d1 == 0)
+  {
+    if (t2 < 0)
+      t2 += a;
+    return t2;
+  }
+  else
+  {
+    if (t1 < 0)
+      t1 += a;
+    return t1;
+  }
 }
