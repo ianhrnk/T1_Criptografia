4b0450421136fff699d7bc2465af981a5d5622d0
Ian Haranaka <ian.hrnk@gmail.com>
2019-11-23 11:01:24 -0400

    fix teodoroForjar

--- src/teodoro.c
+++ src/teodoro.c
@@ -8,11 +8,16 @@ long int teodoroInicializar(int p, int q)
 
 void teodoroAutenticar(long int n, long int* s, long int* v)
 {
-  *s = gerarNumAleatorio(n);
+  *s = gerarNumAleatorio(2, n);
   *v = euclides(n, *s);
 }
 
 long int teodoroForjar(long int n, long int s)
 {
-  return euclides(n, s);
+  long int temp = euclides(n, s);
+  
+  if (verificarCongruencia(n, s, temp))
+    return temp;
+  else
+    return 0;  
 }
